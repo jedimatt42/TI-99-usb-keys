012ef3414b9430fc2cf81b2bd02cab0dc132e7cf
Matthew Splett <matt@cwfk.net>
2015-09-14 23:22:34 -0700

    Beginning to map out the keys.

--- TI99USBKeys/TI99USBKeys.ino
+++ TI99USBKeys/TI99USBKeys.ino
@@ -7,28 +7,99 @@
 #include <SPI.h>
 #endif
 
-int ti_c3 = 6; // key_cable_pin_15
-int ti_c2 = 5; // key_cable_pin_14
-int ti_c1 = 4; // key_cable_pin_13
-int ti_c0 = 3; // key_cable_pin_12
-int ti_r7 = 15; // key_cable_pin_11
-int ti_r6 = 16; // key_cable_pin_10
-int ti_c4 = 2; // key_cable_pin_9
-int ti_c5 = 1; // key_cable_pin_8
-int ti_r4 = 17; // key_cable_pin_7
-int ti_c6 = 0; // key_cable_pin_6
-int ti_r0 = 18; // key_cable_pin_5
-int ti_r1 = 19; // key_cable_pin_4
-int ti_r5 = 20; // key_cable_pin_3
-int ti_r3 = 21; // key_cable_pin_2
-int ti_r2 = 22; // key_cable_pin_1
+// teensy 3.1 pin aliases for TI keyboard connector. 
+//   and info describing the meaning. 
+//   _c? is a column input pin from the TI
+//   _r? is a row output pin for the TI
+int ti_c3 = 6; // key_cable_pin_15  (M J U 7 4 F R V)
+int ti_c2 = 5; // key_cable_pin_14  (, K I 8 3 D E C)
+int ti_c1 = 4; // key_cable_pin_13  (. L O 9 2 S W X)
+int ti_c0 = 3; // key_cable_pin_12  (= space enter fctn shift ctrl)
+int ti_r7 = 15; // key_cable_pin_11 (X C V B Z)
+int ti_r6 = 16; // key_cable_pin_10 (ctrl W E R T Q)
+int ti_c4 = 2; // key_cable_pin_9   (N H Y 6 5 G T B)
+int ti_c5 = 1; // key_cable_pin_8   (/ ; P 0 1 A Q Z)
+int ti_r4 = 17; // key_cable_pin_7  (fctn 2 3 4 5 1 alpha_lock)
+int ti_c6 = 0; // key_cable_pin_6   (alpha_lock)
+int ti_r0 = 18; // key_cable_pin_5  (= . , M N /)
+int ti_r1 = 19; // key_cable_pin_4  (space L K J H ;)
+int ti_r5 = 20; // key_cable_pin_3  (shift S D F G A)
+int ti_r3 = 21; // key_cable_pin_2  (9 8 7 6 0)
+int ti_r2 = 22; // key_cable_pin_1  (enter O I U Y P)
+
+// This data is used to maintain the state of the TI keyboard switches.
+// when a column is scanned by the TI, then the rows are set to match
+// this state. 
+// The state is updated by the key down and key up routines handling
+// the USB keyboard input.
+int c0rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c1rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c2rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c3rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c4rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c5rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c6rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+// map symbols for the address of column/row pairs ( a key on the TI matrix )
+// column 0
+int* tk_Equal = c0rows;
+int* tk_Space = c0rows+1;
+int* tk_Enter = c0rows+2;
+int* tk_Fctn = c0rows+4;
+int* tk_Shift = c0rows+5;
+int* tk_Ctrl = c0rows+6;
+int* tk_Period = c1rows;
+// column 1
+int* tk_L = c1rows+1;
+int* tk_O = c1rows+2;
+int* tk_9 = c1rows+3;
+int* tk_2 = c1rows+4;
+int* tk_S = c1rows+5;
+int* tk_W = c1rows+6;
+int* tk_X = c1rows+7;
+// column 2 (, K I 8 3 D E C)
+int* tk_Comma = c2rows;
+int* tk_K = c2rows+1;
+int* tk_I = c2rows+2;
+int* tk_8 = c2rows+3;
+int* tk_3 = c2rows+4;
+int* tk_D = c2rows+5;
+int* tk_E = c2rows+6;
+int* tk_C = c2rows+7;
+// column 3 (M J U 7 4 F R V)
+int* tk_M = c3rows;
+int* tk_J = c3rows+1;
+int* tk_U = c3rows+2;
+int* tk_7 = c3rows+3;
+int* tk_4 = c3rows+4;
+int* tk_F = c3rows+5;
+int* tk_R = c3rows+6;
+int* tk_V = c3rows+7;
+// column 4 (N H Y 6 5 G T B)
+int* tk_N = c4rows;
+int* tk_H = c4rows+1;
+int* tk_Y = c4rows+2;
+int* tk_6 = c4rows+3;
+int* tk_5 = c4rows+4;
+int* tk_G = c4rows+5;
+int* tk_T = c4rows+6;
+int* tk_B = c4rows+7;
+// column 5 (/ ; P 0 1 A Q Z)
+int* tk_Slash = c5rows;
+int* tk_Semicolon = c5rows+1;
+int* tk_P = c5rows+2;
+int* tk_0 = c5rows+3;
+int* tk_1 = c5rows+4;
+int* tk_A = c5rows+5;
+int* tk_Q = c5rows+6;
+int* tk_Z = c5rows+7;
+// column 6 (alpha_lock)
+int* tk_Alpha = c6rows+4;
+
 
 void setPinModes() 
 {
   setInputs();
-  int outputMode = OUTPUT_OPENDRAIN;
-  setOutputs(outputMode);
-  // for griggles, if these all go to input mode will the TI keyboard work?
   setOutputs(INPUT);
 }
 
@@ -56,39 +127,65 @@ void setOutputs(int outputMode)
   pinMode(ti_r7, outputMode);
 }
 
+//----------------------------------------
+// TI Keyboard Column Scan Handling
+
+void setOutputPin(int pin, int state) 
+{
+  if (state) {
+    pinMode(pin, OUTPUT_OPENDRAIN);
+    digitalWrite(pin, LOW);
+  } else {
+    pinMode(pin, INPUT);
+  }
+}
+
+void setRowOutputs(int* rows)
+{
+  int i = 0;
+  setOutputPin(ti_r0, rows[i++]);
+  setOutputPin(ti_r1, rows[i++]);
+  setOutputPin(ti_r2, rows[i++]);
+  setOutputPin(ti_r3, rows[i++]);
+  setOutputPin(ti_r4, rows[i++]);
+  setOutputPin(ti_r5, rows[i++]);
+  setOutputPin(ti_r6, rows[i++]);
+  setOutputPin(ti_r7, rows[i++]);
+}
+
 void onTiC0()
 {
-  Serial.println("0");
+  setRowOutputs(c0rows);
 }
 
 void onTiC1()
 {
-  Serial.print("1");
+  setRowOutputs(c1rows);
 }
 
 void onTiC2()
 {
-  Serial.print("2");
+  setRowOutputs(c2rows);
 }
 
 void onTiC3()
 {
-  Serial.print("3");
+  setRowOutputs(c3rows);
 }
 
 void onTiC4()
 {
-  Serial.print("4");
+  setRowOutputs(c4rows);
 }
 
 void onTiC5()
 {
-  Serial.print("5");
+  setRowOutputs(c5rows);
 }
 
 void onTiC6()
 {
-  Serial.print("6");
+  setRowOutputs(c6rows);
 }
 
 void setColumnInterrupts()
@@ -103,15 +200,8 @@ void setColumnInterrupts()
   attachInterrupt(ti_c6, onTiC6, interruptMode);
 }
 
-class TIKeys
-{
-  public:
-    volatile int C1 = 0;
-    volatile int R1 = 0;
-};
-
-
-TIKeys tiKeys;
+//-----------------------------------------------
+// USB Keyboard input handling
 
 class KbdRptParser : public KeyboardReportParser
 {
@@ -123,7 +213,6 @@ class KbdRptParser : public KeyboardReportParser
     void OnKeyDown	(uint8_t mod, uint8_t key);
     void OnKeyUp	(uint8_t mod, uint8_t key);
     void OnKeyPressed(uint8_t key);
-
 };
 
 void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
@@ -134,6 +223,7 @@ void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
   Serial.print((mod.bmLeftShift  == 1) ? "S" : " ");
   Serial.print((mod.bmLeftAlt    == 1) ? "A" : " ");
   Serial.print((mod.bmLeftGUI    == 1) ? "G" : " ");
+  Serial.print(m);
 
   Serial.print(" >");
   PrintHex<uint8_t>(key, 0x80);
@@ -149,10 +239,50 @@ void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 {
   Serial.print("DN ");
   PrintKey(mod, key);
-  uint8_t c = OemToAscii(mod, key);
-  
-  if (c)
-    OnKeyPressed(c);
+  switch(mod) {
+    case 64:
+      *tk_Fctn = 1;
+      break;
+  }
+  switch(key) {
+    case 0x28:
+      *tk_Enter = 1;
+      break;
+    case 0x1E:
+      *tk_1 = 1;
+      break;
+    case 0x1F:
+      *tk_2 = 1;
+      break;
+    case 0x2E:
+      *tk_Equal = 1;
+      break;
+  }
+}
+
+void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
+{
+  Serial.print("UP ");
+  PrintKey(mod, key);
+  switch(mod) {
+    case 64:
+      *tk_Fctn = 0;
+    break;
+  }
+  switch(key) {
+    case 0x28:
+      *tk_Enter = 0;
+      break;
+    case 0x1E:
+      *tk_1 = 0;
+      break;
+    case 0x1F:
+      *tk_2 = 0;
+      break;
+    case 0x2E:
+      *tk_Equal = 0;
+      break;
+  }
 }
 
 void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
@@ -190,22 +320,6 @@ void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
   }
 
 }
-
-void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
-{
-  Serial.print("UP ");
-  PrintKey(mod, key);
-  tiKeys.R1 = 0;
-  tiKeys.C1 = 0;
-}
-
-void KbdRptParser::OnKeyPressed(uint8_t key)
-{
-  Serial.print("ASCII: ");
-  Serial.println((char)key);
-  tiKeys.R1 = 1;
-  tiKeys.C1 = 1;
-};
 
 USB     Usb;
 USBHub     Hub(&Usb);
