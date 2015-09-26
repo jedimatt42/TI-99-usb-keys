6ecd9a9335e628a5d3ca640244e4847e64b344b2
Matthew Splett <matt@cwfk.net>
2015-09-25 21:43:46 -0700

    refactor major sections of code out to separate files.

---
+++ TI99USBKeys/TiPins.h
@@ -0,0 +1,141 @@
+#ifndef _TI_PINS_H
+#define _TI_PINS_H
+
+// teensy 3.1 pin aliases for TI keyboard connector. 
+//   and info describing the meaning. 
+//   _c? is a column input pin from the TI
+//   _r? is a row output pin for the TI
+int ti_c3 = 6; // key_cable_pin_15  (M J U 7 4 F R V)
+int ti_c2 = 5; // key_cable_pin_14  (, K I 8 3 D E C)
+int ti_c1 = 4; // key_cable_pin_13  (. L O 9 2 S W X)
+int ti_c0 = 3; // key_cable_pin_12  (= space enter fctn shift ctrl)
+int ti_r7 = 15; // key_cable_pin_11 (X C V B Z)
+int ti_r6 = 16; // key_cable_pin_10 (ctrl W E R T Q)
+int ti_c4 = 2; // key_cable_pin_9   (N H Y 6 5 G T B)
+int ti_c5 = 1; // key_cable_pin_8   (/ ; P 0 1 A Q Z)
+int ti_r4 = 17; // key_cable_pin_7  (fctn 2 3 4 5 1 alpha_lock)
+int ti_c6 = 0; // key_cable_pin_6   (alpha_lock)
+int ti_r0 = 18; // key_cable_pin_5  (= . , M N /)
+int ti_r1 = 19; // key_cable_pin_4  (space L K J H ;)
+int ti_r5 = 20; // key_cable_pin_3  (shift S D F G A)
+int ti_r3 = 21; // key_cable_pin_2  (9 8 7 6 0)
+int ti_r2 = 22; // key_cable_pin_1  (enter O I U Y P)
+
+//-------------------------------------
+// Initialize the TI interfacing pins.
+
+void initPinModes() 
+{
+  initInputs();
+  initOutputs();
+}
+
+void initInputs() 
+{
+  int inputMode = INPUT_PULLUP;
+  pinMode(ti_c0, inputMode);
+  pinMode(ti_c1, inputMode);
+  pinMode(ti_c2, inputMode);
+  pinMode(ti_c3, inputMode);
+  pinMode(ti_c4, inputMode);
+  pinMode(ti_c5, inputMode);
+  pinMode(ti_c6, inputMode);
+}
+
+void initOutputs()
+{
+  // this mode seems to work well with letting the TI builtin keyboard still function.
+  int outputMode = OUTPUT_OPENDRAIN; 
+  pinMode(ti_r0, outputMode);
+  pinMode(ti_r1, outputMode);
+  pinMode(ti_r2, outputMode);
+  pinMode(ti_r3, outputMode);
+  pinMode(ti_r4, outputMode);
+  pinMode(ti_r5, outputMode);
+  pinMode(ti_r6, outputMode);
+  pinMode(ti_r7, outputMode);
+  digitalWrite(ti_r0,HIGH);
+  digitalWrite(ti_r1,HIGH);
+  digitalWrite(ti_r2,HIGH);
+  digitalWrite(ti_r3,HIGH);
+  digitalWrite(ti_r4,HIGH);
+  digitalWrite(ti_r5,HIGH);
+  digitalWrite(ti_r6,HIGH);
+  digitalWrite(ti_r7,HIGH);
+}
+
+//------------------------------------------------------
+// TI Keyboard Column Scan Handling
+//   The following functions are part of the interrupt
+//   handlers triggered when the TI scans a keyboard
+//   column.
+
+void setOutputPin(int pin, int state) 
+{
+  digitalWrite(pin, state ? LOW : HIGH);
+}
+
+// Given a columns set of switches, set each output pin accordingly.
+void setRowOutputs(int* rows)
+{
+  int i = 0;
+  setOutputPin(ti_r0, rows[i++]);
+  setOutputPin(ti_r1, rows[i++]);
+  setOutputPin(ti_r2, rows[i++]);
+  setOutputPin(ti_r3, rows[i++]);
+  setOutputPin(ti_r4, rows[i++]);
+  setOutputPin(ti_r5, rows[i++]);
+  setOutputPin(ti_r6, rows[i++]);
+  setOutputPin(ti_r7, rows[i++]);
+}
+
+void onTiC0()
+{
+  setRowOutputs(c0rows);
+}
+
+void onTiC1()
+{
+  setRowOutputs(c1rows);
+}
+
+void onTiC2()
+{
+  setRowOutputs(c2rows);
+}
+
+void onTiC3()
+{
+  setRowOutputs(c3rows);
+}
+
+void onTiC4()
+{
+  setRowOutputs(c4rows);
+}
+
+void onTiC5()
+{
+  setRowOutputs(c5rows);
+}
+
+void onTiC6()
+{
+  setRowOutputs(c6rows);
+}
+
+void setColumnInterrupts()
+{
+  int interruptMode = FALLING;
+  attachInterrupt(ti_c0, onTiC0, interruptMode);
+  attachInterrupt(ti_c1, onTiC1, interruptMode);
+  attachInterrupt(ti_c2, onTiC2, interruptMode);
+  attachInterrupt(ti_c3, onTiC3, interruptMode);
+  attachInterrupt(ti_c4, onTiC4, interruptMode);
+  attachInterrupt(ti_c5, onTiC5, interruptMode);
+  attachInterrupt(ti_c6, onTiC6, interruptMode);
+}
+
+
+#endif
+
---
+++ TI99USBKeys/TiVirtualState.h
@@ -0,0 +1,95 @@
+#ifndef _TI_VIRTUAL_STATE_H
+#define _TI_VIRTUAL_STATE_H
+
+// This data is used to maintain the state of the TI keyboard switches.
+// when a column is scanned by the TI, then the rows are set to match
+// this state. 
+// The state is updated by the key down and key up routines handling
+// the USB keyboard input.
+int c0rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c1rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c2rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c3rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c4rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c5rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+int c6rows[8] = { 0, 0, 0, 0, 0, 0, 0, 0 };
+
+// map symbols for the address of column/row pairs ( a key on the TI matrix )
+// column 0 (= space enter fctn shift ctrl)
+int* tk_Equal = c0rows;
+int* tk_Space = c0rows+1;
+int* tk_Enter = c0rows+2;
+int* tk_Fctn = c0rows+4;
+int* tk_Shift = c0rows+5;
+int* tk_Ctrl = c0rows+6;
+// column 1 (. L O 9 2 S W X)
+int* tk_Period = c1rows;
+int* tk_L = c1rows+1;
+int* tk_O = c1rows+2;
+int* tk_9 = c1rows+3;
+int* tk_2 = c1rows+4;
+int* tk_S = c1rows+5;
+int* tk_W = c1rows+6;
+int* tk_X = c1rows+7;
+// column 2 (, K I 8 3 D E C)
+int* tk_Comma = c2rows;
+int* tk_K = c2rows+1;
+int* tk_I = c2rows+2;
+int* tk_8 = c2rows+3;
+int* tk_3 = c2rows+4;
+int* tk_D = c2rows+5;
+int* tk_E = c2rows+6;
+int* tk_C = c2rows+7;
+// column 3 (M J U 7 4 F R V)
+int* tk_M = c3rows;
+int* tk_J = c3rows+1;
+int* tk_U = c3rows+2;
+int* tk_7 = c3rows+3;
+int* tk_4 = c3rows+4;
+int* tk_F = c3rows+5;
+int* tk_R = c3rows+6;
+int* tk_V = c3rows+7;
+// column 4 (N H Y 6 5 G T B)
+int* tk_N = c4rows;
+int* tk_H = c4rows+1;
+int* tk_Y = c4rows+2;
+int* tk_6 = c4rows+3;
+int* tk_5 = c4rows+4;
+int* tk_G = c4rows+5;
+int* tk_T = c4rows+6;
+int* tk_B = c4rows+7;
+// column 5 (/ ; P 0 1 A Q Z)
+int* tk_Slash = c5rows;
+int* tk_Semicolon = c5rows+1;
+int* tk_P = c5rows+2;
+int* tk_0 = c5rows+3;
+int* tk_1 = c5rows+4;
+int* tk_A = c5rows+5;
+int* tk_Q = c5rows+6;
+int* tk_Z = c5rows+7;
+// column 6 (alpha_lock)
+int* tk_Alpha = c6rows+4;
+
+
+void clearRow(int* rows)
+{
+  for( int i = 0; i < 8; i++ ) {
+    rows[i] = 0;
+  }
+}
+
+void initData() 
+{
+  fctnEqualsTimestamp = 0L;
+  clearRow(c0rows);
+  clearRow(c1rows);
+  clearRow(c2rows);
+  clearRow(c3rows);
+  clearRow(c4rows);
+  clearRow(c5rows);
+  clearRow(c6rows);
+}
+
+
+#endif
+
