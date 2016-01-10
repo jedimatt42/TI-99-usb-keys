129b18df8b6e3fb882cd4782944dd0e2c29af83e
Matthew Splett <matt@cwfk.net>
2016-01-09 22:48:17 -0800

    Add support for werid keys [/?], [-_], [\|]

--- TI99USBKeys/TiKbdRptParser.h
+++ TI99USBKeys/TiKbdRptParser.h
@@ -62,14 +62,58 @@ void TiKbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after)
   updateModifier(U_LEFTCTRL | U_RIGHTCTRL, before, after, tk_Ctrl);
 }
 
+#define ISSHIFT(X) ((U_LEFTSHIFT | U_RIGHTSHIFT) & X)
+#define ISALT(X) ((U_LEFTALT | U_RIGHTALT) & X)
+#define ISCTRL(X) ((U_LEFTCTRL | U_RIGHTCTRL) & X)
+
 void TiKbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
 {
-  handleSimple(key, 1);
+  if (handleSimple(key, 1)) return;
+
+  if (key == U_HYPHEN && mod == 0) {
+    tk_press(tk_Shift);
+    tk_press(tk_Slash);
+  } else if (key == U_HYPHEN && ISSHIFT(mod)) {
+    tk_release(tk_Shift);
+    tk_press(tk_Fctn);
+    tk_press(tk_U);
+  } else if (key == U_SLASH && !ISSHIFT(mod)) {
+    tk_press(tk_Slash);
+  } else if (key == U_SLASH && ISSHIFT(mod)) {
+    tk_press(tk_Fctn);
+    tk_press(tk_I);
+  } else if (key == U_BACKSLASH && !ISSHIFT(mod)) {
+    tk_press(tk_Fctn);
+    tk_press(tk_Z);
+  } else if (key == U_BACKSLASH && ISSHIFT(mod)) {
+    tk_press(tk_Fctn);
+    tk_press(tk_A);
+  }
 }
 
 void TiKbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
 {
   if (handleSimple(key, 0)) return;
+
+  if (key == U_HYPHEN && mod == 0) {
+    tk_release(tk_Slash);
+    tk_release(tk_Shift);
+  } else if (key == U_HYPHEN && ISSHIFT(mod)) {
+    tk_release(tk_U);
+    tk_release(tk_Fctn);
+    tk_press(tk_Shift);
+  } else if (key == U_SLASH && !ISSHIFT(mod)) {
+    tk_release(tk_Slash);
+  } else if (key == U_SLASH && ISSHIFT(mod)) {
+    tk_release(tk_I);
+    tk_release(tk_Fctn);
+  } else if (key == U_BACKSLASH && !ISSHIFT(mod)) {
+    tk_release(tk_Z);
+    tk_release(tk_Fctn);
+  } else if (key == U_BACKSLASH && ISSHIFT(mod)) {
+    tk_release(tk_A);
+    tk_release(tk_Fctn);
+  }
   
   switch(key) {
     case U_CAPSLOCK:
@@ -78,54 +122,54 @@ void TiKbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
   }
 }
 
-#define SCASE(X, Y) case X: *Y = state; return true;
+#define SCASE(X, Y) case X: *Y = state; return true
 
 // Handle the keys that are a one to one mapping, with no modifier issues.
 boolean TiKbdRptParser::handleSimple(uint8_t key, int state)
 {
   switch(key) {
-    SCASE(U_NUM1,tk_1)
-    SCASE(U_NUM2,tk_2)
-    SCASE(U_NUM3,tk_3)
-    SCASE(U_NUM4,tk_4)
-    SCASE(U_NUM5,tk_5)
-    SCASE(U_NUM6,tk_6)
-    SCASE(U_NUM7,tk_7)
-    SCASE(U_NUM8,tk_8)
-    SCASE(U_NUM9,tk_9)
-    SCASE(U_NUM0,tk_0)
-    SCASE(U_A,tk_A)
-    SCASE(U_B,tk_B)
-    SCASE(U_C,tk_C)
-    SCASE(U_D,tk_D)
-    SCASE(U_E,tk_E)
-    SCASE(U_F,tk_F)
-    SCASE(U_G,tk_G)
-    SCASE(U_H,tk_H)
-    SCASE(U_I,tk_I)
-    SCASE(U_J,tk_J)
-    SCASE(U_K,tk_K)
-    SCASE(U_L,tk_L)
-    SCASE(U_M,tk_M)
-    SCASE(U_N,tk_N)
-    SCASE(U_O,tk_O)
-    SCASE(U_P,tk_P)
-    SCASE(U_Q,tk_Q)
-    SCASE(U_R,tk_R)
-    SCASE(U_S,tk_S)
-    SCASE(U_T,tk_T)
-    SCASE(U_U,tk_U)
-    SCASE(U_V,tk_V)
-    SCASE(U_W,tk_W)
-    SCASE(U_X,tk_X)
-    SCASE(U_Y,tk_Y)
-    SCASE(U_Z,tk_Z)
-    SCASE(U_COMMA,tk_Comma)
-    SCASE(U_PERIOD,tk_Period)
-    SCASE(U_EQUAL,tk_Equal)
-    SCASE(U_SEMICOLON,tk_Semicolon)
-    SCASE(U_SPACE,tk_Space)
-    SCASE(U_ENTER,tk_Enter)
+    SCASE(U_NUM1,tk_1);
+    SCASE(U_NUM2,tk_2);
+    SCASE(U_NUM3,tk_3);
+    SCASE(U_NUM4,tk_4);
+    SCASE(U_NUM5,tk_5);
+    SCASE(U_NUM6,tk_6);
+    SCASE(U_NUM7,tk_7);
+    SCASE(U_NUM8,tk_8);
+    SCASE(U_NUM9,tk_9);
+    SCASE(U_NUM0,tk_0);
+    SCASE(U_A,tk_A);
+    SCASE(U_B,tk_B);
+    SCASE(U_C,tk_C);
+    SCASE(U_D,tk_D);
+    SCASE(U_E,tk_E);
+    SCASE(U_F,tk_F);
+    SCASE(U_G,tk_G);
+    SCASE(U_H,tk_H);
+    SCASE(U_I,tk_I);
+    SCASE(U_J,tk_J);
+    SCASE(U_K,tk_K);
+    SCASE(U_L,tk_L);
+    SCASE(U_M,tk_M);
+    SCASE(U_N,tk_N);
+    SCASE(U_O,tk_O);
+    SCASE(U_P,tk_P);
+    SCASE(U_Q,tk_Q);
+    SCASE(U_R,tk_R);
+    SCASE(U_S,tk_S);
+    SCASE(U_T,tk_T);
+    SCASE(U_U,tk_U);
+    SCASE(U_V,tk_V);
+    SCASE(U_W,tk_W);
+    SCASE(U_X,tk_X);
+    SCASE(U_Y,tk_Y);
+    SCASE(U_Z,tk_Z);
+    SCASE(U_COMMA,tk_Comma);
+    SCASE(U_PERIOD,tk_Period);
+    SCASE(U_EQUAL,tk_Equal);
+    SCASE(U_SEMICOLON,tk_Semicolon);
+    SCASE(U_SPACE,tk_Space);
+    SCASE(U_ENTER,tk_Enter);
   }
   return false;
 }
